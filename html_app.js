2a9f63f4a819314178ddbbfa28605e1cf5f2580c
aspilio <lio.chiron@google.com>
2019-04-27 17:39:27 +0200

    continuing to improve with nunjucks

--- html_app.js
+++ html_app.js
@@ -10,16 +10,6 @@ var util = require('./static/js/util');
 var app = express()
 var server = require('http').createServer(app);
 
-//-------------- Clients
-
-// var html_txt = '/views/strap_small.html'
-// var area_txt = '/views/text.html'
-//
-//
-// app.get('/',function(req,res){ res.sendFile(path.join(__dirname + html_txt)) });
-// app.get('/text',function(req,res){ res.sendFile(path.join(__dirname + area_txt)) });
-
-
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -32,7 +22,6 @@ app.get('/', function(req, res){
 app.get('/text', function(req, res){
     res.render('text.html');
 });
-
 
 //--------------  address
 
@@ -49,58 +38,54 @@ var comment = false;
 
 io.sockets.on('connection', function (socket) {
 
-    console.log('A client is connected!');
-    fs.readFile('views/strap_small.html', 'utf8', function (err,data) {
-            if (err) { return console.log(err); }
+      console.log('A client is connected!');
+      fs.readFile('views/strap_small.html', 'utf8', function (err,data) {
+              if (err) { return console.log(err); }
 
-            socket.emit('message', data); // send the text read in html file to textarea
-            if (comment){ console.log('blablabala in readFile !! ') }
-            // console.log('patt before emit !! ' + patt)
-            // socket.emit('scroll', patt);
-            // console.log('patt after emit !! ' + patt)
+              socket.emit('message', data); // send the text read in html file to textarea
+              if (comment){ console.log('blablabala in readFile !! ') }
+              // console.log('patt before emit !! ' + patt)
+              // socket.emit('scroll', patt);
+              // console.log('patt after emit !! ' + patt)
 
-            var line_number = count.find_line_of_pattern(data, patt)
-            if (comment){ console.log('line_number after find_line_of_pattern !! ' + line_number) }
-            socket.emit('scroll', line_number);
-            if (comment){ console.log('just after scrolllll !! ') }
+              var line_number = count.find_line_of_pattern(data, patt)
+              if (comment){ console.log('line_number after find_line_of_pattern !! ' + line_number) }
+              socket.emit('scroll', line_number);
+              if (comment){ console.log('just after scrolllll !! ') }
 
-        }); // end fs.readFile
+          }); // end fs.readFile
 
-    util.save_regularly()
+      util.save_regularly() // save the regularly the text..
 
-    socket.on('join', function(data) {
-        //var a = 1
-        console.log('client sent a message... ' + data);
-        console.log('value of patt on server side ... ' + patt);
-        socket.emit('scroll',patt)
-      }); // end socket.on join
+      socket.on('join', function(data) {
+          //var a = 1
+          console.log('client sent a message... ' + data);
+          console.log('value of patt on server side ... ' + patt);
+          socket.emit('scroll',patt)
+        }); // end socket.on join
 
-    //-------------------------------- From textarea to html
+      //-------------------------------- From textarea to html
 
-    socket.on('return', function(data) {
+      socket.on('return', function(data) {
 
-          /*
-          Change the text with the text from textarea when submitted
-          */
+            /*
+            Change the text with the text from textarea when submitted
+            */
 
-          console.log('Retrieving the whole text modified... ' + data);
-          fs.writeFile("views/strap_small.html", data, function(err) {
-                if(err) { return console.log(err); }
-                util.save_current_version(data)  // save strap_small.html with a date in saved directory
-                console.log("The file views/strap_small.html was modified and saved!");
+            console.log('Retrieving the whole text modified... ' + data);
+            fs.writeFile("views/strap_small.html", data, function(err) {
+                  if(err) { return console.log(err); }
+                  util.save_current_version(data)  // save strap_small.html with a date in saved directory
+                  console.log("The file views/strap_small.html was modified and saved!");
 
-          }); // end write file
-      }); // end socket.on return
+            }); // end write file
+        }); // end socket.on return
 
       socket.on('scroll', function(pattern) {
           console.log('############ in html_apps pattern is :  ' + pattern)
           patt = pattern
-        })
+          })
 
-        socket.on('pos_scroll', function() {
-            console.log('sending scroll info back !! ')
-            socket.emit('scroll', patt); // send the text read in html file to textarea
-        })
 
 }); // sockets.on connection
 
--- static/js/util.js
+++ static/js/util.js
@@ -55,3 +55,28 @@ exports.save_regularly = function(){
         }, 300000);   // 5 min intervall
 
 }
+
+exports.tab_textarea = function(){
+
+  /*
+  Transforming tab in four spaces
+  */
+
+  $('#comment').keydown(function (e) {
+      var keyCode = e.keyCode || e.which;
+
+      if (keyCode === 9) {
+          e.preventDefault();
+          var start = this.selectionStart;
+          var end = this.selectionEnd;
+
+          // set textarea value to: text before caret + tab + text after caret
+          spaces = "    ";
+          this.value = this.value.substring(0, start) + spaces + this.value.substring(end);
+
+          // put caret at right position again
+          this.selectionStart = this.selectionEnd = start + spaces.length;
+      }
+    });
+
+}
