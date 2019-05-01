ffededb3734e69e03705f0eb262358fd94033a3a
aspilio <lio.chiron@google.com>
2019-05-01 21:19:29 +0200

    cleaning, rewriting etc..

--- html_app.js
+++ html_app.js
@@ -5,7 +5,7 @@ var nunjucks  = require('nunjucks');
 var count = require('./static/js/count_lines');
 var util = require('./static/js/util');
 var re = require('./static/js/read_emit');
-var mh = require('./static/js/modify_html');
+var modify = require('./static/js/modify_html');
 
 //--------------  Server
 
@@ -19,13 +19,8 @@ nunjucks.configure('views', {
     // echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 });
 
-app.get('/', function(req, res){
-    res.render('strap_small.html');
-});
-
-app.get('/text', function(req, res){
-    res.render('text.html');
-});
+app.get('/', function(req, res){ res.render('strap_small.html'); });
+app.get('/text', function(req, res){ res.render('text.html'); });
 
 //--------------  static addresses
 
@@ -49,39 +44,18 @@ io.sockets.on('connection', function (socket) {
               if (err) { return console.log(err); }
               re.emit_from_read(socket, count, patt, text, scroll_html_pos)
           }); // end fs.readFile
-
       //util.save_regularly() // save the regularly the text..
 
-      socket.on('join', function(data) {
-          //var a = 1
-          console.log('client sent a message... ' + data);
-          console.log('value of patt on server side ... ' + patt);
-          socket.emit('scroll', patt)
-
-        }); // end socket.on join
+      socket.on('join', function(data) { socket.emit('scroll', patt) }); // end socket.on join
 
       //-------------------------------- From textarea to html
 
-      socket.on('return', function(new_text) {
-
-            /*
-            Change the text with the text from textarea when submitted
-            */
-
-            mh.modify_html_with_newtext(socket, fs, util, new_text)
-
+      socket.on('return', function(new_text) { // change html with textarea
+            modify.modify_html_with_newtext(socket, fs, util, new_text)
         }); // end socket.on return
 
-      socket.on('scroll', function(pattern) {
-          console.log('############ in html_apps pattern is :  ' + pattern)
-          patt = pattern
-          })
-
-      socket.on('scroll_html', function(pos) {
-          console.log('############ viewed from server side,  scroll html pos is :  ' + pos)
-          scroll_html_pos = pos
-          })
-
+      socket.on('scroll', function(pattern) { patt = pattern })
+      socket.on('scroll_html', function(pos) { scroll_html_pos = pos })
 
 }); // sockets.on connection
 
--- static/js/read_emit.js
+++ static/js/read_emit.js
@@ -8,7 +8,8 @@ exports.emit_from_read = function(socket, count, patt, text, scroll_html_pos){
 
       socket.emit('message', text); // send the text read in html file to textarea
       var line_number = count.find_line_of_pattern(text, patt)
-      socket.emit('scroll', line_number + '+++' + patt);
+      var json_line_patt = JSON.stringify( {'line':line_number, 'pattern':patt} );
+      socket.emit('scroll', json_line_patt);
       socket.emit('scroll_html', scroll_html_pos)
       //if (comment){ console.log('scroll_html_pos ' + scroll_html_pos) }
       socket.emit('pattern', patt); // send the text read in html file to textarea
