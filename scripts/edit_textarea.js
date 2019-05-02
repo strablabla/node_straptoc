0e1a151b8249c1310bf3065f95711c77f7716987
aspilio <lio.chiron@google.com>
2019-05-02 12:14:41 +0200

    factorizing more

--- scripts/edit_textarea.js
+++ scripts/edit_textarea.js
@@ -10,14 +10,14 @@ function wrap_hyperlink(aster, sel){
 
       var subsel = sel.split('\/')
       if (subsel.slice(-1)[0].length < 5){
-         subsel = sel.split('\/').slice(-2,-1) // case final '/'
+         subsel = sel.split('\/').slice(-2,-1)                   // case final '/'
       }
       else{
-          subsel = sel.split('\/').slice(-1)  // case no '/' at the end
+          subsel = subsel.slice(-1)                              // case no '/' at the end
       }
-      var subsubsel = subsel[0].split('.html')[0]
-      var name = '[' + subsubsel + ']'
-      var replace = aster + '* ' + name + '(' + sel + ')' +'\n'
+      var subsubsel = subsel[0].split('.html')[0]                // remove .html for making the name
+      var name = '[' + subsubsel + ']'                           // name
+      var replace = aster + '* ' + name + '(' + sel + ')' +'\n'  // final line
 
       return replace
 
