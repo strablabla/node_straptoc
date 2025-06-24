ea954dec7a76b407ef774724ff6be37b6e2d2db4
lio3867 <{lionel.chiron@gmail.com}>
2025-06-24 11:57:43 +0200

    automation of portfolios

--- static/js/folders.js
+++ static/js/folders.js
@@ -9,6 +9,12 @@ var fs = require('fs');
 
 function deals_with_folder(socket, name_folder){
 
+      /*
+
+      handle a unique folder
+
+      */
+
       var count_pdf = name_folder.split('§§')[1]
       var name_folder = name_folder.split('§§')[0]
 
@@ -22,12 +28,19 @@ function deals_with_folder(socket, name_folder){
 
         console.log('######### sending ' + strap_addr)
         socket.emit('folder_extract', strap_addr)
+
       });
 
 
 }
 
 function deals_with_list_folders(socket, name_folder){
+
+      /*
+
+      handle the list of folders
+
+      */
 
       var count_pdf = name_folder.split('§§')[1]
       var name_folder = name_folder.split('§§')[0]
@@ -43,6 +56,7 @@ function deals_with_list_folders(socket, name_folder){
 
         console.log('######### before sending for list ' + strap_addr)
         socket.emit('list_folders', strap_addr)
+
       });
 
 }
@@ -56,16 +70,16 @@ exports.deals_with_pdfs = function(socket){
 
       socket.on('folder_extract', function(name_folder) {
              console.log('Received the address ' + name_folder)
-             deals_with_folder(socket,name_folder)
+             deals_with_folder(socket, name_folder)
         })
 
       socket.on('list_folders', function(name_folder) {
              console.log('################ addr list_folders.. ' + name_folder)
-             deals_with_list_folders(socket,name_folder)
+             deals_with_list_folders(socket, name_folder)
         })
 
       socket.on('make_elems', function(){             //---- pdf
-            socket.emit('make_elems','')
+            socket.emit('make_elems', '')
         })
 
 
