037e9356a85a4853d84ec903f3636190e0ceeeb0
lio3867 <{lionel.chiron@gmail.com}>
2025-12-30 13:37:28 +0100

    correction on portfolio images disappearing when opening one or clicking on the page locker..

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,3 @@
-var dchandir = {"_chansons":"chansons","_exemples":"exemples","_fait":"fait","_main":"/","_tests":"tests","_todo":"todo"}
-var list_pages = ["chansons","exemples","fait","main","tests","todo"]
-var dic_text_id = {"chansons":"text-#chansons","":"text-#main","exemples":"text-#exemples","fait":"text-#fait","main":"text-#main","tests":"text-#tests","todo":"text-#todo"}
+var dchandir = {"_exemples":"exemples","_main":"/","_todo":"todo"}
+var list_pages = ["exemples","main","todo"]
+var dic_text_id = {"exemples":"text-#exemples","":"text-#main","main":"text-#main","todo":"text-#todo"}
