63e5b701c60fd054785bb214ffde9d332e2b27a9
lio3867 <{lionel.chiron@gmail.com}>
2026-01-07 20:39:06 +0100

    restablishment of integration of the new pages in the pages panel..

--- static/js/new_page.js
+++ static/js/new_page.js
@@ -9,9 +9,12 @@ var init = require('../../lib/init');
 
 exports.create_new_page = function(app, name_page, io){
 
-    make_new_md(name_page)
-    io.sockets.emit('load_new_page', '')
-    init.handle_pages(app)
+    make_new_md(name_page, function(){
+        init.handle_pages(app, function(){
+            console.log("emit load_new_page")
+            io.sockets.emit('load_new_page', '')
+        })
+    })
 
  }
 
@@ -29,7 +32,7 @@ return md_text
 
 }
 
-function make_new_md(title){
+function make_new_md(title, callback){
 
   /*
 
@@ -43,6 +46,7 @@ function make_new_md(title){
     fs.writeFile(addr, md_text, function(err) {   // create the template
           if(err) { return console.log(err); }
           console.log("The file was saved at {} !".format(addr));
+          if(callback){ callback() }
     });    // end writeFile
 
 }
